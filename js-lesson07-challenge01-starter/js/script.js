9fc9a1597123d911fec6c0d4fb61b698487e6adc
Ranelle_I <43350178+legendoflilac@users.noreply.github.com>
2023-10-05 16:38:27 -0500

    Added keydown event listeners

--- js-lesson07-challenge01-starter/js/script.js
+++ js-lesson07-challenge01-starter/js/script.js
@@ -3,11 +3,27 @@ var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
 button.addEventListener("click", function () {
-  modal.classList.add("show-modal");
-  button.innerText = "You've got this!!!";
+  openModal();
 });
 
 modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
+})
+
+var closeModal = function() {
   modal.classList.remove("show-modal");
   button.innerText = "Who's got this?";
-});
+}
+
+var openModal = function() {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+}
