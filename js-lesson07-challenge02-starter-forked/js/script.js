e0853eff4a37b1654f0286aa15e999efb5e74142
Ranelle_I <43350178+legendoflilac@users.noreply.github.com>
2023-10-09 16:25:54 -0500

    Added dropdown functionality

--- js-lesson07-challenge02-starter-forked/js/script.js
+++ js-lesson07-challenge02-starter-forked/js/script.js
@@ -1,1 +1,32 @@
 // Your JS code here
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic"; //Default value for subscription drop down
+var subDuration = 1; //Default value for duration drop down
+
+subTypeElement.addEventListener("change", function(e){
+    subType = e.target.value;
+    updateSubscriptionDiv();
+    //console.log(subType);
+})
+
+subDurationElement.addEventListener("change", function(e){
+    subDuration = Number(e.target.value);
+    updateSubscriptionDiv();
+    //console.log(subDuration);
+})
+
+var updateSubscriptionDiv = function() {
+    var monthlyCost = 5; //default cost for basic
+    console.log(subType);
+    if (subType === "standard") {
+        monthlyCost = 7;
+    }
+    else if (subType === "premium") {
+        monthlyCost = 10;
+    }
+
+    var total = subDuration * monthlyCost
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
