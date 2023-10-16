14ad192fae93b031d2a20be5b837604d59fff951
Ranelle_I <43350178+legendoflilac@users.noreply.github.com>
2023-10-16 08:47:26 -0500

    Removed files to add to separate repository

--- js-l12-challenge-01-main/js-l12-challenge-01-main/js/script.js
+++
@@ -1,4 +0,0 @@
-const button = document.querySelector("button");
-const authorSpan = document.querySelector(".author");
-const imgDiv = document.querySelector(".image-container");
-const img = document.querySelector(".img");
