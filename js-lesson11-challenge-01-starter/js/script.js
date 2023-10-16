c2c5999d34d207751dbb5a200d3a5a9d87bd1492
Ranelle_I <43350178+legendoflilac@users.noreply.github.com>
2023-10-16 06:56:40 -0500

    Added function to show pet statuses

--- js-lesson11-challenge-01-starter/js/script.js
+++ js-lesson11-challenge-01-starter/js/script.js
@@ -1,16 +1,16 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
 
-const createPet = function(name, species) {
+const createPet = function (name, species) {
     const pet = {
         name: name,
         species: species,
         isTired: 5,
-        sleep: function() {
+        sleep: function () {
             console.log(`${this.name} needs a nap. Zzz...`);
             this.isTired = 1;
         },
-        play: function() {
+        play: function () {
             if (this.isTired === 10) {
                 console.log(`Too tired to play.`);
                 this.sleep();
@@ -32,7 +32,32 @@ const francine = createPet("Francine", "turtle");
 
 //console.log(sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+//clover.sleep();
+//baxter.play();
 
 //console.log(baxter, clover);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+//console.log(allPets);
+
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping.";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+
+    }
+}
+
+statusButton.addEventListener("click", function () {
+    showPets(allPets);
+})
